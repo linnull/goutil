713a9b6777d2335fe417dbc7c89da478c5ed405b
Lin YuBin <linnull@163.com>
2017-02-18 22:53:35 +0800

    添加获得网址超链接和图片链接的方法

--- goutil.go
+++ goutil.go
@@ -3,12 +3,17 @@ package goutil
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Data struct {
 	Data []string
 }
 
+//从一个json文件读取数据，返回字符串数组，json文件格式如下：
+//{"Data":["xx","xx","xx",...]}
 func GetStringsFromJson(jsonName string) ([]string, error) {
 	b, err := ioutil.ReadFile(jsonName)
 	if err != nil {
@@ -20,5 +25,53 @@ func GetStringsFromJson(jsonName string) ([]string, error) {
 		return nil, err
 	}
 	return items.Data, nil
+
+}
+
+//判断文件是否存在
+func IsExist(fileName string) bool {
+	_, err := os.Stat(fileName)
+	return err == nil || os.IsExist(err)
+}
+
+//获得一个网址的所有图片链接
+func GetImagesLinks(url string) ([]string, error) {
+	links := []string{}
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return nil, err
+	}
+	sel := doc.Find("img")
+
+	for _, n := range sel.Nodes {
+
+		for _, i := range n.Attr {
+			if i.Key == "src" {
+				links = append(links, i.Val)
+			}
+		}
+	}
+	return links, nil
+
+}
+
+//获得一个网址的所有超链接
+func GetHyperlinks(url string) ([]string, error) {
+	links := []string{}
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return nil, err
+	}
+	sel := doc.Find("a")
+
+	for _, n := range sel.Nodes {
+
+		for _, i := range n.Attr {
+			if i.Key == "href" {
+				links = append(links, i.Val)
+			}
+		}
+	}
+	return links, nil
 
 }
--- goutil_test.go
+++ goutil_test.go
@@ -1,15 +1,55 @@
 package goutil
 
 import (
+	"errors"
 	// "log"
 	"testing"
 )
 
 func TestGetStringsFromJson(t *testing.T) {
 
+	// items, err := GetStringsFromJson("testdata/test.json")
 	_, err := GetStringsFromJson("testdata/test.json")
 	if err != nil {
 		t.Error(err)
 	}
 	// log.Println(items)
+
+	_, err = GetStringsFromJson("testdata/xxx.json")
+	if err == nil {
+		t.Error(err)
+	}
+	// log.Println(err)
+}
+
+func TestIsExist(t *testing.T) {
+
+	b := IsExist("testdata/test.json")
+	if !b {
+		t.Error(errors.New("file is exist,test is fail"))
+	}
+
+	b = IsExist("testdata/xxx.json")
+	if b {
+		t.Error(errors.New("file is not exist,test is fail"))
+	}
+}
+
+func TestGetImagesLinks(t *testing.T) {
+	// links, err := GetImagesLinks("http://www.163.com")
+	_, err := GetImagesLinks("http://www.163.com")
+	if err != nil {
+		t.Error(err)
+	}
+	// log.Println(links)
+}
+
+func TestGetHyperlinks(t *testing.T) {
+
+	// links, err := GetHyperlinks("http://www.163.com")
+	_, err := GetHyperlinks("http://www.163.com")
+	if err != nil {
+		t.Error(err)
+	}
+	// log.Println(links)
 }
